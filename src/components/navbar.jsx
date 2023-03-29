6669ddd737095d2dfe5631bc8aabee12cc3ebd8d
Md Imran Raj <mdimranraj786@gmail.com>
2023-03-30 01:19:36 +0530

    links changed to NavLink

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import "../styles/navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-            <Link className="navbar-brand" to="#">Cassette</Link>
+            <NavLink className="navbar-brand" to="/">Cassette</NavLink>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav navbar-right-items">
-                <Link className="nav-link" to="/">Home</Link>
-                <Link className="nav-link" to="/developer">Developer</Link>
-                <Link className="nav-link" to="/history">History</Link>
+                <NavLink className="nav-link" to="/">Home</NavLink>
+                <NavLink className="nav-link" to="/developer">Developer</NavLink>
+                <NavLink className="nav-link" to="/history">History</NavLink>
                 
             </div>
             </div>
